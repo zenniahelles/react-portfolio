562aa6e7b21df082d0581d6adda6763b24e73872
zenniahelles <44374280+zenniahelles@users.noreply.github.com>
2020-03-19 11:17:48 +0100

    Add files via upload

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
 import './Projects.scss';
-import Slider from '../Slider/Slider.jsx';
 
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+ 
+const slider = (
+  <AwesomeSlider>
+    <div data-src="/Images/photo1.jpeg" />
+    <div data-src="/Images/photo2.jpeg" />
+    <div data-src="/Images/photo3.jpeg" />
+    <div data-src="/Images/photo4.jpeg" />
+    <div data-src="/Images/photo5.jpeg" />
+  </AwesomeSlider>
+);
 
 const Projects = props => {
     return (
         <>
         <div id="Projects" className="ProjectsContainer"><h2>PROJECTS</h2>
-        <Slider />
+        {slider}
         </div>
         </>
     )
