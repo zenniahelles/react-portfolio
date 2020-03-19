f56f4214ec96feba935181f2bd74be62c14c500c
zenniahelles <44374280+zenniahelles@users.noreply.github.com>
2020-03-19 14:20:40 +0100

    Add files via upload

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Download.scss';
+import {ReactComponent as Butterfly} from '../../images/butterfly.svg';
 
 
 const Download = props => {
     return (
         <>
-        <div id="Download" className="DownloadContainer"><h2>DOWNLOAD CV</h2></div>
+        <div id="Download" className="DownloadContainer">
+            
+            <h2>DOWNLOAD CV</h2>
+            <section className="download-grid">
+                <Butterfly className="butterfly"/>
+                <div className="buttons">
+                    <button>DOWNLOAD VC</button>
+                    <button>SE I GOOGLE DRIVE</button>
+                    </div>
+            </section>
+        
+        </div>
         </>
     )
 }
