c56763f1fc2e4666f397d284ef720b7a69c746fe
zenniahelles <44374280+zenniahelles@users.noreply.github.com>
2020-03-19 09:17:39 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import TopMenu from './components/TopMenu/TopMenu.jsx';
 import TopArea from './components/TopArea/TopArea.jsx';
 import Icons from './components/Icons/Icons.jsx';
-
+import Projects from './components/Projects/Projects.jsx';
+import Download from './components/Download/Download.jsx';
+import ContactMe from './components/Contact/Contact.jsx';
+import Slider from './components/Slider/Slider.jsx';
 
 //APP------------------------------------------------//
 
@@ -12,7 +15,11 @@ function App() {
     <>
     <TopMenu />
     <TopArea />
-      <Icons />
+    <Icons />
+    <Projects />
+    <Slider />
+    <Download />
+    <ContactMe />
     </>
   );
 }
